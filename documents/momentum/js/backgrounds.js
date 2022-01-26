958ca123846a489b3bddb09bb3fbdb743f49ecdd
yehjin-jo <94041147+yehjin-jo@users.noreply.github.com>
2022-01-26 12:36:28 +0900

    Delete documents/momentum directory

--- documents/momentum/js/backgrounds.js
+++
@@ -1,9 +0,0 @@
-const images = ["01.JPG", "02.JPG", "03.JPG"];
-
-const chosenImage = images[Math.floor((Math.random() * images.length))];
-
-const bodyStyle = document.body.style;
-
-bodyStyle.backgroundImage= `url('img/${chosenImage}')`;
-bodyStyle.backgroundPosition = "center center";
-bodyStyle.backgroundOrigin = "content-box";
